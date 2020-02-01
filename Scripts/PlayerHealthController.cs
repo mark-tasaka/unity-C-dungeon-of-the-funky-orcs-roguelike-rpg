3e6b0679d00ac77ae5fb3bc7a2b3bd3f940ff696
Mark Tasaka <mark.tasaka@gmail.com>
2020-01-31 22:36:53 -0800

    Update 1.02.1

---
+++ Scripts/PlayerHealthController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+
+    public static PlayerHealthController instance;
+
+    public int currentHitPoints;
+
+    public int maxHitPoints;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHitPoints = maxHitPoints;
+        UIController.instance.hitPointBar.maxValue = maxHitPoints;
+        UIController.instance.hitPointBar.value = currentHitPoints;
+        UIController.instance.hitPointText.text = currentHitPoints.ToString() + " / " + maxHitPoints.ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DamageToPlayer()
+    {
+        //to be changed; place holder
+        currentHitPoints -= 3;
+
+        UIController.instance.hitPointBar.value = currentHitPoints;
+        UIController.instance.hitPointText.text = currentHitPoints.ToString() + " / " + maxHitPoints.ToString();
+        
+    }
+}
