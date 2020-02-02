e79a338f60e578411b076dae48e71fcc930bb83f
Mark Tasaka <mark.tasaka@gmail.com>
2020-02-01 21:38:29 -0800

    Update 1.03

---
+++ UIController.cs
@@ -0,0 +1,32 @@
+﻿/*Dungeon of the Funky Orcs
+ * A rogue-like RPG adventures designed by Mark Tasaka 2020
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Library for UI elements
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+    public Slider hitPointBar;
+    public Text hitPointText;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
