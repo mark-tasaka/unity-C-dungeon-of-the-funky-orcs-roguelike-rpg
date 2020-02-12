f95f9fd8c5f2248c831b5099bc807435dba1af9d
Mark Tasaka <mark.tasaka@gmail.com>
2020-02-11 21:44:57 -0800

    Update 1.12

---
+++ Scripts/AnimationController.cs
@@ -0,0 +1,28 @@
+﻿/*Dungeon of the Funky Orcs
+ * A rogue-like RPG adventures designed by Mark Tasaka 2020
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+
+    public Animator anim;
+
+    //lower the value, the slower the animation
+    public float AnimSpeed = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim.speed = AnimSpeed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
