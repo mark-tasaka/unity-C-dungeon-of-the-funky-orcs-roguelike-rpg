f6482ec135cec1d33071174dd5dee8153763a9d5
Mark Tasaka <mark.tasaka@gmail.com>
2020-02-15 23:15:18 -0800

    Update 1.14

---
+++ Scripts/AudioTitleManager.cs
@@ -0,0 +1,30 @@
+﻿/*Dungeon of the Funky Orcs
+ * A rogue-like RPG adventures designed by Mark Tasaka 2020
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTitleManager : MonoBehaviour
+{
+    public static AudioTitleManager instance;
+
+    public AudioSource titleMusic;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿/*Dungeon of the Funky Orcs
+ * A rogue-like RPG adventures designed by Mark Tasaka 2020
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string LevelToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(LevelToLoad);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
