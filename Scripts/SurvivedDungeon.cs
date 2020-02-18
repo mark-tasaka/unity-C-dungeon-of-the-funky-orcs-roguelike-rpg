434dceb0f683bd7ada240439c96e1d5d9db0ef31
Mark Tasaka <mark.tasaka@gmail.com>
2020-02-17 23:28:25 -0800

    Update 1.16

---
+++ Scripts/SurvivedDungeon.cs
@@ -0,0 +1,48 @@
+﻿/*Dungeon of the Funky Orcs
+ * A rogue-like RPG adventures designed by Mark Tasaka 2020
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//For Scene Manager
+using UnityEngine.SceneManagement;
+
+public class SurvivedDungeon : MonoBehaviour
+{
+
+    public float waitForAnyKey = 2.0f;
+
+    public GameObject anyKeyText;
+
+    public string loadScene;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waitForAnyKey > 0)
+        {
+            waitForAnyKey -= Time.deltaTime;
+
+            if (waitForAnyKey <= 0)
+            {
+                anyKeyText.SetActive(true);
+            }
+        }
+        else
+        {
+            //covers all key enters
+            if (Input.anyKeyDown)
+            {
+                SceneManager.LoadScene(loadScene);
+            }
+        }
+
+    }
+}
