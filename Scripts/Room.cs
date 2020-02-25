69a407dee24586cb382960c3b9b372444b81695c
Mark Tasaka <mark.tasaka@gmail.com>
2020-02-24 20:35:46 -0800

    Update 1.21

--- Scripts/Room.cs
+++ Scripts/Room.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+   
     // Start is called before the first frame update
     void Start()
     {
---
+++ Scripts/RoomCentre.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomCentre : MonoBehaviour
+{
+    /*
+    public Room theRoom;
+
+    public List<GameObject> enemies = new List<GameObject>();
+    */
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //spawn enemies here
+
+        //add reference to Room.cs
+        
+    }
+}
