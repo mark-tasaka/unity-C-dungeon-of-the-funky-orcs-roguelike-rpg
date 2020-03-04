fee86d4235bd1b3015c9b58ab44c412b7379c540
Mark Tasaka <mark.tasaka@gmail.com>
2020-03-03 20:36:52 -0800

    Update 1.28

---
+++ Scripts/TestScript/TestCameraFollowPlayer.cs
@@ -0,0 +1,31 @@
+﻿/*Dungeon of the Funky Orcs
+ * A rogue-like RPG adventures designed by Mark Tasaka 2020
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCameraFollowPlayer : MonoBehaviour
+{
+    public Transform playerCharacter;
+    public float cameraDistance = 50.0f;
+
+    private void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);
+    }
+    
+    /*
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(playerCharacter.position.x, playerCharacter.position.y, transform.position.z);
+    }
+    */
+
+        
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(playerCharacter.position.x, playerCharacter.position.y, transform.position.z);
+    }
+
+}
